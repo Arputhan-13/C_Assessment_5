629068a4a7bc7f70ac9cd9ad73a18822714d5ced
Arputhan-13 <karputhan@gmail.com>
2026-06-07 22:17:26 +0530

    Create C_problem_21.c

---
+++ C_problem_21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b, c, d, e=0, i;
+    printf("Enter the Number : ");
+    scanf("%d",&a);
+    for(i=a;a!=0;a=a/10)
+    {
+    b=a%10;
+    c=(a/10)%10;
+    d=c*10+b;
+    if(d%2==1)
+    {
+    e=e+1;
+    }
+    }
+    printf("%d",e);
+}
