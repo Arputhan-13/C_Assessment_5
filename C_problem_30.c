949f52af6c4cac8a800aeff2b204ca5cd25f7071
Arputhan-13 <karputhan@gmail.com>
2026-06-07 22:54:08 +0530

    Create C_problem_30.c

---
+++ C_problem_30.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int a, b,c, d, e, i;
+    printf("Enter the number 1 :");
+    scanf("%d",&a);
+    printf("Enter the number 2 :");
+    scanf("%d",&b);
+    if(a>b)
+    {
+    c=a;
+    d=b;
+    }
+    else
+    {
+    c=b;
+    d=a;
+    }
+    for(i=c;d!=0;)
+    {
+    e=c%d;
+    c=d;
+    d=e;
+    }
+    printf("%d",c);
+}
